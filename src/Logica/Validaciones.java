0cfa7cd76e5cc3dde1490d04ef3c6007de02bf8c
Jeff <Jeff@Jeferson>
2026-07-02 10:15:03 -0500

    Interfaz grafica con 2 capas

--- src/Logica/Validaciones.java
+++ src/Logica/Validaciones.java
@@ -26,4 +26,18 @@ public class Validaciones {
     public boolean esPositivo(double numero) {
         return numero > 0;
     }
+    
+    //Metodo a comprobar si la tecla ingresada es de control
+    private static boolean esCaracterDeControl(java.awt.event.KeyEvent evt) {
+        char car = evt.getKeyChar();
+        return Character.isISOControl(car) || 
+               evt.getKeyCode() == java.awt.event.KeyEvent.VK_DELETE ||
+               evt.getKeyCode() == java.awt.event.KeyEvent.VK_BACK_SPACE;
+    } 
+    
+    //Sobrecarga de esNumero destinado para eventos
+    public static boolean esNumero(java.awt.event.KeyEvent evt) {
+        char car = evt.getKeyChar();
+        return Character.isDigit(car) || esCaracterDeControl(evt);
+    } 
 }
